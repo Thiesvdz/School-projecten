cfaaae58ef2698ada2b5d9c916a7cee6741befcc
Thies Van der zon <71008930+Thiesvdz@users.noreply.github.com>
2020-09-30 14:02:24 +0200

    Add files via upload

---
+++ ditbenik.py
@@ -0,0 +1,116 @@
+print ("Hello You!, ik ben Thies")
+print("Wie ben jij?")
+
+naam = input("Wat is jouw naam?:")
+print("Hallo: " + naam)
+
+print("----------------------------------------")
+
+print("Ik ben Thies")
+
+print("----------------------------------------")
+
+print ("Ik ben een nieuwkomer op deze school,en ik heb hier een klein quizje om mij te leren kennen")
+
+
+print("----------------------------------------")
+
+print ("Vraag 1: Waar woon ik?")
+
+print("A: Tuitjehorn")
+print("B: Warmenhuizen")
+print("C: Alkmaar")
+
+antwoord = input()
+
+if antwoord == "A":
+    print("Nee, dit is fout")
+    print ("Het goede antwoord is B: Warmenhuizen")
+
+if antwoord == "B":
+    print ("Dit is goed, ik woon in Warmenhuizen")
+    print ("Op naar de volgende vraag")
+
+if antwoord == "C":
+    print("Nee, dit is fout")
+    print ("Het goede antwoord is B: Warmenhuizen")
+
+
+print("----------------------------------------")
+
+
+print ("Vraag 2: Hoe oud ben ik?")
+
+print("A: 15")
+print("B: 16")
+print("C: 17")
+
+antwoord = input()
+
+if antwoord == "A":
+    print("Dit is goed, ik ben 15 jaar oud.")
+    
+if antwoord == "B":
+    print ("Dit is fout, Het goede antwoord is A: 15")
+
+if antwoord == "C":
+    print ("Dit is fout, Het goede antwoord is A: 15")
+
+
+print("----------------------------------------")
+
+
+
+print("Vraag 3: Wat is mijn favoriete serie")
+
+print("A: Hoops")
+print("B: Lucifer")
+print("C: Friends")
+
+antwoord = input()
+
+if antwoord == "A":
+    print ("Dit is fout, Het goede antwoord is C: Friends")
+  
+if antwoord == "B":
+    print ("Dit is fout, Het goede antwoord is C: Friends")
+
+if antwoord == "C":
+    print ("Dit goed, mijn favoriete serie is Friends")
+
+
+print("----------------------------------------")
+
+
+print("Tijd voor een spelletje ")
+
+
+print("----------------------------------------")
+
+print("Je loopt door Gothem City, je moet naar de overkant van de straat dus je besluit te J-walken. Dan zie je daar Batman en realiseert dat die waarschijnlijk de laatste keer is dat je loopt")
+
+
+print("----------------------------------------")
+
+
+print("Wat doe je?, Vechten of Rennen ")
+
+antwoord = input()
+
+if antwoord == "Vechten":
+    print ("Je besluit je grond te staan en hem uit te dagen, Foute keuze hij breekt je ruggegraat in 25 stukjes.")
+    print ("Je overleeft het niet :(")
+
+elif antwoord == "Rennen":
+    print ("Je rent zo hard als je kan en eindigt in een steegje, je denkt hem kwijt te zijn door achter een vuilnisbak te verstoppen")
+    print ("Toch niet, Ineens staat hij voor je Wat doe je?")
+
+else:
+    print (antwoord,"INVALID")
+
+print("----------------------------------------")
+
+#if antwoord 'Rennen' 
+    #print("test")
+
+
