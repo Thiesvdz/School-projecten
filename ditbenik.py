351e34f489f31f5c138a2187176df0faa97644c1
Thies Van der zon <71008930+Thiesvdz@users.noreply.github.com>
2020-10-06 14:40:49 +0200

    Add files via upload

--- ditbenik.py
+++ ditbenik.py
@@ -82,35 +82,86 @@ if antwoord == "C":
 print("----------------------------------------")
 
 
-print("Tijd voor een spelletje ")
+print(" ")
 
 
 print("----------------------------------------")
 
-print("Je loopt door Gothem City, je moet naar de overkant van de straat dus je besluit te J-walken. Dan zie je daar Batman en realiseert dat die waarschijnlijk de laatste keer is dat je loopt")
+print("Waar wil je het nu over hebben?  ")
 
 
 print("----------------------------------------")
 
 
-print("Wat doe je?, Vechten of Rennen ")
+print("Je wordt wakker met een kater, JONKO of GLAS WATER")
+keuze = input()
+if keuze == 'JONKO':
+    print("Goeie keuze dikke toeter wordt geconsumeerd...")
 
-antwoord = input()
-
-if antwoord == "Vechten":
-    print ("Je besluit je grond te staan en hem uit te dagen, Foute keuze hij breekt je ruggegraat in 25 stukjes.")
-    print ("Je overleeft het niet :(")
-
-elif antwoord == "Rennen":
-    print ("Je rent zo hard als je kan en eindigt in een steegje, je denkt hem kwijt te zijn door achter een vuilnisbak te verstoppen")
-    print ("Toch niet, Ineens staat hij voor je Wat doe je?")
+elif keuze == 'GLAS WATER':
+    print("Prima, je bent weer gehydrateerd tijd voor een jonko!")
 
 else:
-    print (antwoord,"INVALID")
+    print (keuze, "INVALID")
 
-print("----------------------------------------")
+print("________________________________________________________________")
 
-#if antwoord 'Rennen' 
-    #print("test")
+print("Je moet naar school, hoe ga je heen? BUS of PITBIKE")
+keuze = input()
+
+if keuze == 'BUS':
+    print("Helaas! bus gemist, Dan maar met de pitbike")
+
+elif keuze == 'PITBIKE':
+    print("Dikke wheelies trekken op de pitbike best gaan.")
+
+else:
+    print (keuze, "INVALID")
+
+print("________________________________________________________________")
+
+print("Je komt op school net optijd in de les, Je hebt alleen helemaal geen zin in school, SLAPEN of UIT DE LES GAAN")
+keuze = input()
+
+if keuze == 'SLAPEN':
+    print("Je gaat liggen op je tafel en valt in slaap, alleen wordt je al 5 min later wakker met een docent die voor je neus staat te schreeuwen, dus je wordt uit de les gestuurd")
+
+
+elif keuze == 'UIT DE LES GAAN':
+    print("Je bent er alweer klaar mee en besluit weg te gaan, de docent vraagt wat je gaat doen dus je bedenkt een simpel smoesje")
+
+else:
+    print (keuze, "INVALID")
+
+print("________________________________________________________________")
+    
+print("Je bent weer buiten, pakt de pitbike, doet een burnout op het plein crost vol weg. Onderweg naar huis kom je een donerzaak en hebt wel honger, DONER of NAAR HUIS")
+keuze = input()
+
+if keuze == 'DONER':
+    print("Je neemt een lekker broodje doner met saus, als je klaar bent ga je weer lekker door naar huis")
+
+elif keuze == 'NAAR HUIS':
+    print("Je hebt niet zo'n zin in doner gaat rijd door")
+
+else:
+    print (keuze, "INVALID")
+
+print("________________________________________________________________")
+
+print("Je bent weer thuis gaat lekker op de bank zitten en zet de tv aan, BIER of JONKO")
+keuze = input()
+
+if keuze == 'BIER':
+    print("Je tikt een koud pilsie open en geniet van je vrije dag")
+
+elif keuze == 'JONKO':
+    print("Je draait een dikke toeter een gaat lekker smoken op de bank")
+
+else:
+    print (keuze, "INVALID")
+
+
+
 
 
