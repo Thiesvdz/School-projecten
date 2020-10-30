235cd7c87e685fec786f6b4dadfb78da377850de
Thies <Thiesvanderzon@gmail.com>
2020-10-30 11:16:23 +0100

    bijna klaar

--- Text-Based aplicatie.py
+++ Text-Based aplicatie.py
@@ -36,7 +36,7 @@ def beginverhaal():
 
 def verhaalstuk1():
     os.system("cls")
-    print("Je rijdt naar rechts maar je hebt geen benzine meer, dus moet weer verder gaan lopen. Na een tijdje lopen ben je moe en besluit een pauze te nemen.\n")
+    print("Je rijdt naar rechts maar je hebt geen benzine meer, dus moet weer verder gaan lopen. Na een tijdje lopen ben je moe en besluit een pauze te nemen.\nNa de pauze heb je weer genoeg energie om door te gaan\n")
     antwoord = input()
     if antwoord == 'A':
         print("-")
@@ -78,14 +78,12 @@ def verhaalstuk3():
         print(antwoord, "INVALID")
         
 def verhaalstuk4():
-    os.system("cls")
-    print("Je komt een 2 groepen mensen tegen, Je bedenkt dat het misschien wel handig is dat je met hen meet gaat\nGroep 1 gaat met de auto \nGROEP 1 = A, GROEP 2 = B")
+    #os.system("cls")
+    print("Uiteindelijk kom je 2 groepen mensen tegen, Je bedenkt dat het misschien wel handig is dat je met hen meet gaat\nGroep 1 gaat met de auto en Groep 2 = gaat lopend voor minder risico \nGROEP 1 = A, GROEP 2 = B")
     antwoord = input()
     if antwoord == 'A':
-        print("-")
         verhaalstuk10()
     elif antwoord == 'B':
-        print("-")
         verhaalstuk8()
     else:
         print(antwoord, "INVALID")
@@ -93,14 +91,8 @@ def verhaalstuk4():
 def verhaalstuk5():
     os.system("cls")
     print("Je gaat zoeken naar je vrienden,je vindt uiteindelijk een goede vriend van je en gaat samen met hem vluchten.\nSamen eindigen jullie in een vluchtenlingenkamp hopend op een beter leven")
-    antwoord = input()
-    if antwoord == 'A':
-        print("")   
-    elif antwoord == 'B':
-        print("")
-    else:
-        print(antwoord, "INVALID")
 
+    
 def verhaalstuk6():
     #os.system("cls")
     print("Je hebt besloten om de richting van de kust te gaan, Je herrinert je dat er een stad is richting de kust, Wil je er naartoe? JA = A NEE = B  ") 
@@ -128,18 +120,20 @@ def verhaalstuk7():
         
 def verhaalstuk8():
     os.system("cls")
-    print("Je gaat met groep 2 mee naar het blijkt dat. Je vraagt waar ze precies naar toe gaan, ze vertellen je over een bootsmokkel plek waar je het land uit kan gaan")
+    print("Je gaat met groep 2 mee naar het blijkt dat. Je vraagt waar ze precies naar toe gaan, ze vertellen je over een bootsmokkel plek waar je het land uit kan gaan. \nJe weet het niet zeker of dat slim is want er zit een best groot risico achter. Iemand uit de groep verteld dat hij misschien een plek weet waar je via een vrachtwagen het land uit kan gaan.\nWat doe je HAVEN = A, WEGGAAN = B")
     antwoord = input()
     if antwoord == 'A':
-        print("")   
+        print("_")
+        verhaalstuk9()
     elif antwoord == 'B':
-        print("")
+        print("_")
+        verhaalstuk15()
     else:
         print(antwoord, "INVALID")
         
 def verhaalstuk9():
     os.system("cls")
-    print("Je komt aan bij de haven en voelt je geweldig, maar het is nog niet af.\nJe moet een groot bedrag betalen en hebt net aan genoeg maar je wilt niet al je geld kwijt. gelukkig is er ook een goedkopere optie voor een andere boot Wat doe je?\nDURE BOOT = A , GOEDKOPERE BOOT = B")
+    print("Je komt aan bij de haven en zit al wat mensen vertrekken die ook vluchten, maar het is nog niet af.\nJe moet een groot bedrag betalen en hebt net aan genoeg maar je wilt niet al je geld kwijt. gelukkig is er ook een goedkopere optie voor een andere boot Wat doe je?\nDURE BOOT = A , GOEDKOPERE BOOT = B")
     antwoord = input()
     if antwoord == 'A':
         print("")
@@ -152,13 +146,11 @@ def verhaalstuk9():
         
 def verhaalstuk10():
     os.system("cls")
-    print("Je hebt gekozen voor groep 1 uit, je vraagt waar je naartoe gaat, Ze zeggen dat je naar de haven gaat en met een boot naar een ander land gaat.\nJe kan ook kiezen om niet naar de haven te gaan en weer lopend verder\nHAVEN = A, UITSTAPPEN = B")
+    print("Je hebt gekozen voor groep 1 uit, je vraagt waar je naartoe gaat, Ze zeggen dat er bij de haven een bootsmokkelplek is, daar kan je een boot naar een ander land nemen.\nJe kan ook kiezen om niet naar de haven te gaan en weer lopend verder\nHAVEN = A, UITSTAPPEN = B")
     antwoord = input()
     if antwoord == 'A':
-        print("")
         verhaalstuk9()
     elif antwoord == 'B':
-        print("")
         verhaalstuk15()
     else:
         print(antwoord, "INVALID")
@@ -168,9 +160,9 @@ def verhaalstuk11():
     print("Je zet door een hoopt voor het best, maar helaas haal je het niet en vriest dood") 
     antwoord = input()
     if antwoord == 'A':
-        print("")   
+        print("_")
     elif antwoord == 'B':
-        print("")
+        print("_")
     else:
         print(antwoord, "INVALID")
         
@@ -179,10 +171,8 @@ def verhaalstuk12():
     print("Je vindt het te riskant en besluit om niet mee te gaan en gaat zelf door. Je gaat een besluit maken, richting de kust te gaan of landinwaarts trekken\nKUST = A , LAND INWAARST = B")
     antwoord = input()
     if antwoord == 'A':
-        print("")
         verhaalstuk6()
     elif antwoord == 'B':
-        print("")
         verhaalstuk3()
     else:
         print(antwoord, "INVALID")
@@ -192,10 +182,8 @@ def verhaalstuk13():
     print("Je gaat naar rechts, en gaat bedenken of je ergens bij vrienden naartoe kan of is het toch slimmer om alleen te blijven en risico's te vermijden. Wat doe je? ANDEREN ZOEKEN = A, ALLEEN VERDER = B")
     antwoord = input()
     if antwoord == 'A':
-        print("")
         verhaalstuk5()
     elif antwoord == 'B':
-        print("")
         verhaalstuk19()
     else:
         print(antwoord, "INVALID")
@@ -207,10 +195,8 @@ def verhaalstuk14():
     print("Je gaat naar links, en ziet je auto staan. Wat doe je? INSTAPPEN = A, VERDER OP VOET = B")
     antwoord = input()
     if antwoord == 'A':
-        print("----")
         verhaalstuk2()
     elif antwoord == 'B':
-        print("----")
         verhaalstuk13()
     else:
         print(antwoord, "INVALID")
@@ -218,66 +204,53 @@ def verhaalstuk14():
 #Einde1
 def verhaalstuk15():
     os.system("cls")
-    print("Je bent uitgestapt en gaat bedenken wat je kan gaan doen, voordat je het weet wordt je opgepakt door ISIS./n je wordt geforceerd om te vechten voor hen")
-   
-    else:
-        print(antwoord, "INVALID")
+    print("Je bent weg bij de groep gegaan en gaat bedenken wat je het beste kan doen, voordat je het weet wordt je opgepakt door ISIS./n je wordt geforceerd om te vechten voor hen")
+    Enterknop()
+    
 #Einde3
 def verhaalstuk16():
     os.system("cls")
     print("Je neemt de goedkope boot en denkt dat alles goedkomt, helaas slaat de boot om de een storm op zee en overleef je dit niet")
-  
+    Enterknop()
 
 #Einde1
 def verhaalstuk17():
     os.system("cls")
     print("Je gaat overnachten en gaat door in de ochtend, je maakt een lange dat je net aan overleeft komt aan in een vluchtenlingenkamp")
-    antwoord = input()
-    if antwoord == 'A':
-        print("")
-        
-    elif antwoord == 'B':
-        print("")
-         
-    else:
-        print(antwoord, "INVALID")
+    Enterknop()
 
 def verhaalstuk18():
     os.system("cls")
     print("Je bent naar links gegaan en na een lange reis kom je bij een smokkelbedrijf langs, de smokkelaars verdelen de mensen in groepen, je probeert één van de smokkelaars op te kopen Welke groep wil je in?\nGROEP 1 = A, GROEP 2 = B")
     antwoord = input()
     if antwoord == 'A':
-        print("")
         verhaalstuk10()
     elif antwoord == 'B':
-        print("")
         verhaalstuk8()
     else:
-        print(antwoord, "INVALID")
+        print(antwoord, "= INVALID")
 
 def verhaalstuk19():
     os.system("cls")
     print("Je hebt gekozen om alleen verder te gaan, Je gaat richting het Zuiden waar je hoopt dat het velig is.\nJe komt een groepje andere vluchtelingen tegen, hun plan is om over de grens proberen te komen en ze vragen je mee Wat doe je? NIET MEEGAAN = A, MEEGAAN =B")
     antwoord = input()
     if antwoord == 'A':
-        print("-")
         verhaalstuk12()
     elif antwoord == 'B':
-        print("-")
         verhaalstuk7()
     else:
-        print(antwoord, "INVALID")
+        print(antwoord, "= INVALID")
 
 def verhaalstuk20():
     os.system("cls")
-    print("Je maakt het veilig naar europa, daar kan je een veilig en gelukkig leven te gemoet gaan")
-
-    else:
-        print(antwoord, "INVALID")
+    print("Na een lange en moeilijke reis kom je uiteindelijk veilig aan in europa, daar kan je een veilig en gelukkig leven te gemoet gaan")
+    Enterknop()
+    
+    
 #Einde1
 def verhaalstuk21():
     os.system("cls")
-    print("Je gaat rechtdoor en na een lange reis eindig je veiling in een vluchtelingenkamp")
+    print("Je gaat rechtdoor en na een lange reis eindig je veiling in een vluchtelingenkamp, Hier zal je de rest van je leven doorbrengen")
     Enterknop()
 
 gameRunning = True
@@ -289,6 +262,8 @@ while gameRunning == True:
         continue
     elif antwoord == 'NEE':
         break
-    
+    else:
+        antwoord == ("= INVALID")
+        break
 
 
